b7879466f2f5d41ed969a55949c3f9f295eddaf4
Masum1x <masumislam57888@gmail.com>
2026-08-30 09:49:34 +0600

    Connect app to public matchmaking server

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SERVER_URL = 'http://127.0.0.1:3000';
+const SERVER_URL = 'https://randomvideoapps.onrender.com';
 
 export const socket = io(SERVER_URL, {
   autoConnect: false,
